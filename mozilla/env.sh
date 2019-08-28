dc62741511eb62db34765ea17fa00507952a49a0
Perry Jiang <me@perryjiang.com>
2019-08-28 10:04:32 -0700

    Modularize mozconfigs

--- mozilla/env.sh
+++ mozilla/env.sh
@@ -5,7 +5,13 @@ alias mach="$HOME/src/mozilla-unified/mach"
 export PATH="$HOME/.mozbuild/arcanist/bin:$HOME/.mozbuild/moz-phab:$PATH"
 
 export HGRCPATH="$DOTFILES/mozilla/.hgrc"
-export MOZCONFIG="$DOTFILES/mozilla/mozconfig"
+export MOZCONFIGS_DIR="$DOTFILES/mozilla/mozconfigs"
+
+if [[ "$(uname)" == "Linux" ]]; then
+  export MOZCONFIG="$MOZCONFIGS_DIR/mozconfig.linux"
+elif [[ "$(uname)" == "Darwin" ]]; then
+  export MOZCONFIG="$MOZCONFIGS_DIR/mozconfig.macos"
+fi
 
 # Suppress {++,--}{DOCSHELL,DOMWINDOW} debugging output
 export MOZ_QUIET=1
